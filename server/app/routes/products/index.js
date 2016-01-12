ccf65cea26670eb118c188d836ec7ee8d8f756f6
mliou8 <chunghanliou@gmail.com>
2016-01-12 16:15:12 -0500

    pushing up to github so andrew can continue working on products routes

---
+++ server/app/routes/products/index.js
@@ -0,0 +1,17 @@
+var router = require('express').Router();
+module.exports = router;
+var mongoose = require('mongoose');
+
+var Product = mongoose.model('Product');
+
+router.get('/', function(){
+
+})
+
+router.get('/:id', function(){
+
+})
+
+router.get('/:category', function(){
+
+})
