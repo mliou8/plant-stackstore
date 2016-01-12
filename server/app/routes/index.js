89972d6ebba55b0c0644833a791a6f57e8bd4b13
Brian Dunn <briancollierdunn@gmail.com>
2016-01-12 15:44:14 -0500

    created user, cart, and review schemas and user and review routes

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -3,6 +3,8 @@ var router = require('express').Router();
 module.exports = router;
 
 router.use('/members', require('./members'));
+router.use('/user', require('./user'));
+router.use('/review', require('./review'));
 
 // Make sure this is after all of
 // the registered routes!
