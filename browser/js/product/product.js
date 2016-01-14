40b00c3ca69c6e4d46695fba4688b6bb1aee1d9d
Brian Dunn <briancollierdunn@gmail.com>
2016-01-14 17:34:15 -0500

    added form on product page for adding to cart

--- browser/js/product/product.js
+++ browser/js/product/product.js
@@ -9,6 +9,9 @@ app.config(function($stateProvider) {
             },
             reviews: function(ProductFactory, $stateParams) {
                 return ProductFactory.fetchReviewsById($stateParams.productId);
+            },
+            user: function(AuthService) {
+                return AuthService.getLoggedInUser();
             }
         }
     });
@@ -17,4 +20,20 @@ app.config(function($stateProvider) {
 app.controller('ProductCtrl', function($scope, product, reviews) {
     $scope.product = product;
     $scope.reviews = reviews;
+    var range = function(start,end) {
+        var result = [];
+        for(var i = start; i <= end && i <= 30; i++) {
+            result.push({
+                name:i,
+                val:i
+            });
+        }
+        return result;
+    }
+    $scope.quantities = range(1,product.stock);
+    $scope.amount = 1;
+
+    $scope.addToCart = function() {
+        console.log($scope.amount,$scope.product._id);
+    }
 })
