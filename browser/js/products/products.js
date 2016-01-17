d39f557f2f3210a7896484ec4dd3b3fd2bc13ab4
mliou8 <chunghanliou@gmail.com>
2016-01-17 02:56:18 -0500

    Added in capacity to search by category, review, or product name

--- browser/js/products/products.js
+++ browser/js/products/products.js
@@ -6,6 +6,19 @@ app.config(function ($stateProvider) {
         resolve: {
             allProducts: function(ProductFactory) {
                 return ProductFactory.fetchAll();
+<<<<<<< Updated upstream
+||||||| merged common ancestors
+            },
+            allReviews: function(ProductFactory) {
+                return ProductFactory.fetchAllReviews();
+=======
+            },
+            allReviews: function(ProductFactory) {
+                return ProductFactory.fetchAllReviews();
+            },
+            allCategories: function(ProductFactory) {
+                return ProductFactory.fetchAllCategories();
+>>>>>>> Stashed changes
             }
             // },
             // searchDB: function(SearchFactory) {
@@ -15,9 +28,20 @@ app.config(function ($stateProvider) {
     });
 });
 
+<<<<<<< Updated upstream
 app.controller('ProductsCtrl', function($scope, allProducts, ProductFactory) {
     $scope.products = allProducts;
+||||||| merged common ancestors
+app.controller('ProductsCtrl', function($scope, allProducts, ProductFactory, allReviews) {
+    //Returns the products scope object and maps onto it
+    //two new properties. The average of the reviews, and how many there are
+=======
+app.controller('ProductsCtrl', function($scope, allCategories, allProducts, ProductFactory, allReviews) {
+    //Returns the products scope object and maps onto it
+    //two new properties. The average of the reviews, and how many there are
+>>>>>>> Stashed changes
 
+<<<<<<< Updated upstream
     $scope.filterFunc = function (data) {
 
     }
@@ -28,6 +52,35 @@ app.controller('ProductsCtrl', function($scope, allProducts, ProductFactory) {
             return function( item ) {
                 return item.name === criteria.name;
             };
+||||||| merged common ancestors
+    $scope.products = allProducts.map(function (product) {
+        product.reviews = allReviews.filter(function (review) {
+            return review.product._id === product._id
+        })
+        var sum = 0;
+        product.reviewLength = product.reviews.length;
+        product.reviews.forEach(function (review) {
+            sum = sum + review.rating
+=======
+    $scope.products = allProducts.map(function (product) {
+        product.reviews = allReviews.filter(function (review) {
+            return review.product._id === product._id
+        })
+        product.categories = "";
+        //Roundabout way to map product category into "names" instead of
+        // ids
+        for (var i = 0; i < product.category.length; i++) {
+            for (var j = 0; j < allCategories.length; j++) {
+                if (allCategories[j]._id === product.category[i]) {
+                    product.category[i] = allCategories[j].name;
+                }
+            }
+        }
+        var sum = 0;
+        product.reviewLength = product.reviews.length;
+        product.reviews.forEach(function (review) {
+            sum = sum + review.rating
+>>>>>>> Stashed changes
         })
     };
 });
@@ -56,13 +109,26 @@ app.factory('ProductFactory', function($http) {
             return $http.post('api/review', data)
             .then(function(response){
                 return response.data;
+<<<<<<< Updated upstream
             })
+||||||| merged common ancestors
+            });
+=======
+            });
+        },
+        fetchAllCategories: function() {
+            return $http.get('api/categories')
+            .then (function (response) {
+                return response.data;
+            });
+>>>>>>> Stashed changes
         }
         // getallProducts: function (name) {
         //     return $http.post('api/products', name)
         // }
 
     }
+<<<<<<< Updated upstream
 });
 
 // app.factory('SearchFactory', function ($http) {
@@ -81,3 +147,9 @@ app.factory('ProductFactory', function($http) {
 //         }
 //     }
 // })
+||||||| merged common ancestors
+})
+=======
+})
+
+>>>>>>> Stashed changes
